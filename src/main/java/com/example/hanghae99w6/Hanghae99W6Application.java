303e5072b78fff5b9601c141eeba929fb0fa5bb1
1argeD <94172354+1argeD@users.noreply.github.com>
2022-08-12 11:14:45 +0900

    Initial commit

---
+++ src/main/java/com/example/hanghae99w6/Hanghae99W6Application.java
@@ -0,0 +1,13 @@
+package com.example.hanghae99w6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghae99W6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hanghae99W6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hanghae99w6/Hanghae99W6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanghae99w6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hanghae99W6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
